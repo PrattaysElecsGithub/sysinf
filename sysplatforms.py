da460893745b1926b2b6bd2df3b8341cb3396192
Prattay Sarkar <orginalprattaysarkar@gmail.com>
2023-07-30 16:03:13 +0530

    Description Change 2

--- sysplatforms.py
+++ sysplatforms.py
@@ -3,3 +3,5 @@ import pwd
 import os
 
 # todo: continue, you are making a py lib for returning system info.
+# url 1: https://chat.openai.com/c/6cdd5f68-7317-407a-83ec-7e6cf12bea43
+# url 2: https://chat.openai.com/c/b2248b97-7d39-4200-a6bd-212adc13f61e
