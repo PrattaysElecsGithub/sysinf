d7304e5389f77ce980ccdf31711e41dd9929d8bd
Prattay Sarkar <orginalprattaysarkar@gmail.com>
2023-07-30 16:00:10 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='math_operations',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    description='A simple library for getting system info.',
+    author='Prattay Sarkar',
+    author_email='orginalprattayelec@gmail.com',
+    url='https://github.com/yourusername/math_operations',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ sysplatforms.py
@@ -0,0 +1,5 @@
+import platform
+import pwd
+import os
+
+# todo: continue, you are making a py lib for returning system info.
